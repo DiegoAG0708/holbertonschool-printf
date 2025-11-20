9c675528c3c3242a633db2b2e9249ec90effc4f4
root <root@ip-10-42-201-67.ec2.internal>
2025-11-20 14:25:35 +0000

    Task 1: add %d and %i integer handling to _printf

--- _printf.c
+++ _printf.c
@@ -4,7 +4,7 @@
 *print_char - Writes a single character to stdout
 *@c: The character to print
 *
-*Return: Number of characters written (1), or -1 on error
+*Return: 1 on success, -1 on error
 */
 static int print_char(char c)
 {
@@ -17,9 +17,7 @@ return (1);
 *print_string - Writes a string to stdout
 *@s: The string to print
 *
-*Return: Number of characters written
-*
-*Description: If s is NULL, prints "(null)".
+*Return: Number of characters written, or -1 on error
 */
 static int print_string(const char *s)
 {
@@ -40,21 +38,53 @@ return (count);
 }
 
 /**
+*print_int - Writes an integer to stdout
+*@n: The integer to print
+*
+*Return: Number of characters written, or -1 on error
+*/
+static int print_int(int n)
+{
+int count = 0;
+unsigned int num;
+
+if (n < 0)
+{
+if (print_char('-') == -1)
+return (-1);
+count++;
+num = -n;
+}
+else
+{
+num = n;
+}
+
+if (num / 10)
+{
+int sub = print_int(num / 10);
+if (sub == -1)
+return (-1);
+count += sub;
+}
+
+if (print_char((num % 10) + '0') == -1)
+return (-1);
+count++;
+
+return (count);
+}
+
+/**
 *_printf - Produces output according to a format
-*@format: A character string with zero or more directives
+*@format: A character string with directives
 *
-*Return: The number of characters printed
-*
-*Description: Handles conversion specifiers:
-* c: character
-* s: string
-* %: percent sign
+*Return: Number of characters printed, or -1 on error
 */
 int _printf(const char *format, ...)
 {
 va_list ap;
-int i = 0, total = 0;
-int wrote;
+int i = 0, total = 0, wrote;
 
 if (format == NULL)
 return (-1);
@@ -62,7 +92,6 @@ return (-1);
 va_start(ap, format);
 
 while (format[i])
-
 {
 if (format[i] != '%')
 {
@@ -87,51 +116,32 @@ return (-1);
 if (format[i] == 'c')
 {
 wrote = print_char((char)va_arg(ap, int));
-if (wrote == -1)
-{
-va_end(ap);
-return (-1);
-}
-total += wrote;
 }
 else if (format[i] == 's')
 {
 wrote = print_string(va_arg(ap, char *));
-if (wrote == -1)
-{
-va_end(ap);
-return (-1);
-}
-total += wrote;
 }
 else if (format[i] == '%')
 {
 wrote = print_char('%');
-if (wrote == -1)
-{
-va_end(ap);
-return (-1);
 }
-total += wrote;
+else if (format[i] == 'd' || format[i] == 'i')
+{
+wrote = print_int(va_arg(ap, int));
 }
 else
 {
 wrote = print_char('%');
-if (wrote == -1)
-{
-va_end(ap);
-return (-1);
+if (wrote != -1)
+wrote += print_char(format[i]);
 }
-total += wrote;
 
-wrote = print_char(format[i]);
 if (wrote == -1)
 {
 va_end(ap);
 return (-1);
 }
 total += wrote;
-}
 i++;
 }
 }
